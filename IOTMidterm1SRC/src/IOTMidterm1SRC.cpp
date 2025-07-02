6bd0336e8a5d5d9ee2d5832fb86b5074df2e446f
Phylicia Bediako <phylicia.bediako@gmail.com>
2025-07-02 09:33:20 -0600

    Neopixel code basics added

--- IOTMidterm1SRC/src/IOTMidterm1SRC.cpp
+++ IOTMidterm1SRC/src/IOTMidterm1SRC.cpp
@@ -14,10 +14,9 @@
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-const int PIXELCOUNT = 10;
-int j, i;
+const int PIXELCOUNT = 9;
 int color;
-int startPixel, endPixel;
+int startPixelTmp, endPixelTmp, startPixelHu, endPixelHu, startPixelWt, endPixelWt;
 
 void pixelFill(int startPixel, int endPixel, int color);
 
@@ -25,6 +24,8 @@ Adafruit_NeoPixel pixel (PIXELCOUNT, SPI1, WS2812B);
 
 
 void setup() {
+  Serial.begin(9600);
+  waitFor(Serial.isConnected, 10000);
   pixel.begin();
   pixel.setBrightness(35);
   pixel.show();
@@ -32,17 +33,32 @@ void setup() {
 }
 
 void loop() {
-  for(i=0; i<=10; i++){
-    startPixel=0;
-    endPixel= 10;
-    pixelFill(startPixel, endPixel, violet);
-  }
+//Temperature Neopixels
+    startPixelTmp=0;
+    endPixelTmp= 2;
+    pixelFill(startPixelTmp, endPixelTmp, violet);
+
+  
+//Humidity Neopixels
+    startPixelHu=3;
+    endPixelHu=5;
+    pixelFill(startPixelHu, endPixelHu, green);
+ 
+
+//Weight Neopixels
+    startPixelWt=6;
+    endPixelWt=8;
+    pixelFill(startPixelWt, endPixelWt, red);
+ 
 }
 
 void pixelFill(int startPixel, int endPixel, int color){
-  for(j=startPixel; j<=endPixel; j=j+1){
-    pixel.setPixelColor(j, color);
+  int i;
+  for(i=startPixel; i<=endPixel; i=i+1){
+    pixel.setPixelColor(i, color);
   }
   pixel.show();
-  //delay(100);
+
 }
+
+
