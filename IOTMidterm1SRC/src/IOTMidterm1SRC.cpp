9e4f46e75508fb31f7e0919c06d037a723fb8fd9
Phylicia Bediako <phylicia.bediako@gmail.com>
2025-07-04 18:26:22 -0600

    Design elements

--- IOTMidterm1SRC/src/IOTMidterm1SRC.cpp
+++ IOTMidterm1SRC/src/IOTMidterm1SRC.cpp
@@ -15,6 +15,7 @@
 #include "Adafruit_SSD1306.h"
 #include "Adafruit_BME280.h"
 #include "IoTTimer.h"
+#include "Encoder.h"
 
 //Scanning for BME, OLED, maybe Load Cell Amp
 byte status, address, nDevices;
@@ -22,12 +23,23 @@ byte status, address, nDevices;
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
 const int PIXELCOUNT = 9;
+const float MAXENCPOS = 96;
+
+const int PINA=D8;
+const int PINB=D9;
+
+const int ENCSWITCH = D15;
+
+bool switchState1, switchState2, switchClicked;
+
 int color;
 int startPixelTmp, endPixelTmp, startPixelHu, endPixelHu, startPixelWt, endPixelWt;
+int encPos, lastEncPos;
 
 void pixelFill(int startPixel, int endPixel, int color);
 
 Adafruit_NeoPixel pixel (PIXELCOUNT, SPI1, WS2812B);
+Button encSwitch(ENCSWITCH);
 
 
 void setup() {
@@ -45,13 +57,16 @@ void setup() {
 void loop() {
   Serial.printf("Starting scan: \n-------------\n");
   nDevices=0;
-  fir(address=0; address <=127; address++){
+  for(address=0; address <=127; address++){
     Wire.beginTransmission(address);
     status=Wire.endTransmission();
     if(status == 0);
   }
+  if (encSwitch.isClicked()){
+    switchClicked = !switchClicked;
 
-  ////Temperature Neopixels
+  }
+////Temperature Neopixels
   startPixelTmp=0;
   endPixelTmp= 2;
   pixelFill(startPixelTmp, endPixelTmp, violet);
@@ -60,15 +75,20 @@ void loop() {
 ////Humidity Neopixels
   startPixelHu=3;
   endPixelHu=5;
-  pixelFill(startPixelHu, endPixelHu, green);
+  if (humidity > 50.00()){
+    pixelFill(startPixelHu, endPixelHu, red);
+  }
+  else{
+    pixelFill(startPixelHu, endPixelHu, green);
+  }
 
 
 ////Weight Neopixels
   startPixelWt=6;
   endPixelWt=8;
-  pixelFill(startPixelWt, endPixelWt, red);
-
-}
+  if(weight<=2.00()){
+    pixelFill(startPixelWt, endPixelWt, red);
+  }
 
 void pixelFill(int startPixel, int endPixel, int color){
   int i;
