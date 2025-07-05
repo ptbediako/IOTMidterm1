007a05c4bce9cc85d04bd9ba483d160c1482d523
Phylicia Bediako <phylicia.bediako@gmail.com>
2025-07-04 22:28:37 -0600

    Added code

--- IOTMidterm1SRC/src/IOTMidterm1SRC.cpp
+++ IOTMidterm1SRC/src/IOTMidterm1SRC.cpp
@@ -17,10 +17,9 @@
 #include "IoTTimer.h"
 #include "Encoder.h"
 
-//Scanning for BME, OLED, maybe Load Cell Amp
-byte status, address, nDevices;
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
 
 const int PIXELCOUNT = 9;
 const float MAXENCPOS = 96;
@@ -39,64 +38,109 @@ int encPos, lastEncPos;
 void pixelFill(int startPixel, int endPixel, int color);
 
 Adafruit_NeoPixel pixel (PIXELCOUNT, SPI1, WS2812B);
+Adafruit_BME280 bme;
+const int OLED_RESET = -1;
+Adafruit_SSD1306 display(OLED_RESET);
+IoTTimer measTimer;
 Button encSwitch(ENCSWITCH);
 
+float tempC, humidRH;
+float tempF, weight;
+bool status;
+const char degree = 0xF8;
+const int TIMER = 500;
+const int BME280=0x76;
+const int OLEDSCRN=0x3c;
+
+const int WEMO1=1;
+const int WEMO2=2;
+const int WEMO3=3;
+const int WEMO4=4;
+const int WEMO5=5;
+const int WEMO0=0;
+
+bool onOff;
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
   waitFor(Serial.isConnected, 10000);
-  Serial.printf("I2C Scanner\n") //part of scanning for I2C devices
 
+  status=bme.begin(BME280);
+  if (status == false){
+    Serial.printf("BME280 at address 0x%02x failed to start",BME280);
+  }
+  display.begin(SSD1306_SWITCHCAPVCC,OLEDSCRN);
+  display.display();
+  display.clearDisplay();
+  measTimer.startTimer();
+  
   //pixel.begin();
   //pixel.setBrightness(35);
   //pixel.show();
   
 }
-
+/********************************************************
+*********************************************************
+*********************************************************/
 void loop() {
-  Serial.printf("Starting scan: \n-------------\n");
-  nDevices=0;
-  for(address=0; address <=127; address++){
-    Wire.beginTransmission(address);
-    status=Wire.endTransmission();
-    if(status == 0);
-  }
-  if (encSwitch.isClicked()){
-    switchClicked = !switchClicked;
 
-  }
-////Temperature Neopixels
-  startPixelTmp=0;
-  endPixelTmp= 2;
-  pixelFill(startPixelTmp, endPixelTmp, violet);
+  if(measTimer.isTimerReady()){
+    tempC=bme.readTemperature();
+    tempF=(tempC*1.8) +32;
+    humidRH=bme.readHumidity();
+    //Don't forget to add weight reading here
 
-  
-////Humidity Neopixels
-  startPixelHu=3;
-  endPixelHu=5;
-  if (humidity > 50.00()){
-    pixelFill(startPixelHu, endPixelHu, red);
-  }
-  else{
-    pixelFill(startPixelHu, endPixelHu, green);
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    //Don't forget to add weight to the display print
+    display.printf("Temperature: %0.1f %cF\nHumidity: %0.1f RH\n",tempF, degree, humidRH))
+    display.display();
+
+    measTimer.startTimer(TIMER);
+    
+
+    if (encSwitch.isClicked()){
+      switchClicked = !switchClicked;
+
+    }
+  ////Temperature Neopixels
+    startPixelTmp=0;
+    endPixelTmp= 2;
+    if (tempF > 72.00()){
+    pixelFill(startPixelTmp, endPixelTmp, red);
+    }
+
+    
+  ////Humidity Neopixels
+    startPixelHu=3;
+    endPixelHu=5;
+    if (humidRH > 50.00()){
+      pixelFill(startPixelHu, endPixelHu, red);
+    }
+    else{
+      pixelFill(startPixelHu, endPixelHu, green);
+    }
+
+
+  ////Weight Neopixels
+    startPixelWt=6;
+    endPixelWt=8;
+    if(weight<=2.00()){
+      pixelFill(startPixelWt, endPixelWt, red);
+    }
   }
 
 
-////Weight Neopixels
-  startPixelWt=6;
-  endPixelWt=8;
-  if(weight<=2.00()){
-    pixelFill(startPixelWt, endPixelWt, red);
-  }
-
+/********************************************************
+*********************************************************
+*********************************************************/
 void pixelFill(int startPixel, int endPixel, int color){
   int i;
   for(i=startPixel; i<=endPixel; i=i+1){
     pixel.setPixelColor(i, color);
   }
   pixel.show();
-
 }
-
 
